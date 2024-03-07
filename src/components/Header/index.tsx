eafe331da8bf47de03f5639c50c7c05f7a4889ee
Maia <matteusmaia889@gmail.com>
2024-03-07 15:40:16 -0300

    criando tipagem para props e utilizando a propriedade para criar uma condicional se mostra o BackIcon ou não

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 import logoImg from "../../assets/logo.png";
-import { CaretLeft } from "phosphor-react-native";
 
-export function Header() {
+type HeaderProps = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: HeaderProps) {
   return (
     <Container>
-      <BackButton>
-        <BackIcon />
-      </BackButton>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+
       <Logo source={logoImg} />
     </Container>
   );
