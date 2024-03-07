fa4c10c29a5c3221073dee56ffd6352b57e19268
Maia <matteusmaia889@gmail.com>
2024-03-07 15:34:39 -0300

    importando e utilizando os componentes estilizados

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
-import { Container, Logo } from "./styles";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 import logoImg from "../../assets/logo.png";
+import { CaretLeft } from "phosphor-react-native";
 
 export function Header() {
   return (
     <Container>
+      <BackButton>
+        <BackIcon />
+      </BackButton>
       <Logo source={logoImg} />
     </Container>
   );
