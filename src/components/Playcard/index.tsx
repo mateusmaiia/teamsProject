308f5b3ee2c77860138412f6577c1ccd48a4602a
Maia <matteusmaia889@gmail.com>
2024-03-09 21:18:47 -0300

    Criação do componente PlayCard

---
+++ src/components/Playcard/index.tsx
@@ -0,0 +1,16 @@
+import { ButtonIcon } from "../ButtonIcon";
+import { Container, Icon, Name } from "./styles";
+
+type Props = {
+  name: string;
+  onRemove: () => void;
+};
+export function PlayCard({ name, onRemove }: Props) {
+  return (
+    <Container>
+      <Icon name="person" />
+      <Name>{name}</Name>
+      <ButtonIcon onPress={onRemove} icon="close" type="SECONDARY" />
+    </Container>
+  );
+}
