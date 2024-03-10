f51f037965b66d5f5eca7e4a9db04c2bad4a4576
Maia <matteusmaia889@gmail.com>
2024-03-10 01:18:17 -0300

    Adicionei  a funcionalidade de voltar para a tela inicial

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,21 @@
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 import logoImg from "../../assets/logo.png";
+import { useNavigation } from "@react-navigation/native";
 
 type HeaderProps = {
   showBackButton?: boolean;
 };
 
 export function Header({ showBackButton = false }: HeaderProps) {
+  const navigation = useNavigation();
+
+  function handleGoBackHome() {
+    navigation.navigate("groups");
+  }
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoBackHome}>
           <BackIcon />
         </BackButton>
       )}
