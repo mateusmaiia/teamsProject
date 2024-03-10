f3fc5c8383c3a052d2ac904fd5c89c4231564af3
Maia <matteusmaia889@gmail.com>
2024-03-10 01:17:52 -0300

    arrumei os erros na digitação dos nomes

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -2,9 +2,9 @@ export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       //coloca as rotas e a tipagem é o parametro que a rota/componente tem
-      grous: undefined;
-      newGrous: undefined;
-      player: {
+      groups: undefined;
+      newGroup: undefined;
+      players: {
         group: string;
       };
     }
