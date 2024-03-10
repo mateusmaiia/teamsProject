ea7319a87630e46918332b528fbfc91c4cafe945
Maia <matteusmaia889@gmail.com>
2024-03-09 22:57:19 -0300

    tipagem das rotas de navegação

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      //coloca as rotas e a tipagem é o parametro que a rota/componente tem
+      grous: undefined;
+      newGrous: undefined;
+      player: {
+        group: string;
+      };
+    }
+  }
+}
